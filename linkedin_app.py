b4fa280b3a31a7814691b3a2a0eacce9cd379aa7
Viewtest <94213692+Viewtest-Coding-Team@users.noreply.github.com>
2024-03-23 02:06:27 +0700

    Update linkedin_app.py

--- linkedin_app.py
+++ linkedin_app.py
@@ -68,4 +68,4 @@ def linkedin_callback():
 
 if __name__ == '__main__':
     # Run the application
-    linkedin_app.run(debug=True)
+    linkedin_app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
